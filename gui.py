0b80bbdaf9b28bd237984e9287480fabeb1b3abd
Sergey Zadyabin <zsergt3@gmail.com>
2018-11-12 22:01:38 +0300

    add search box (wo functionality yet)

--- gui.py
+++ gui.py
@@ -49,19 +49,25 @@ class SearchBox(urwid.Frame):
 
 class CtrlButtons(urwid.Columns):
     def __init__(self):
-        self.home_but = urwid.Padding(urwid.Button('Home'), 'center', 8)
-        self.align_but = urwid.Padding(urwid.Button('Align'), 'center', 9)
-        self.list_but = urwid.Padding(urwid.Button('List'), 'center', 8)
-        self.search_but = urwid.Padding(urwid.Button('Search'), 'center', 10)
+        self.home_but = urwid.Button('Home')
+        self.align_but = urwid.Button('Align')
+        self.list_but = urwid.Button('List')
+        self.search_but = urwid.Button('Search')
         super().__init__((
-            self.home_but,
-            self.align_but,
-            self.list_but,
-            self.search_but
+            urwid.Padding(self.home_but, 'center', 8),
+            urwid.Padding(self.align_but, 'center', 9),
+            urwid.Padding(self.list_but, 'center', 8),
+            urwid.Padding(self.search_but, 'center', 10),
             ))
 
 
 class TopFrame(urwid.Frame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # import ipdb; ipdb.set_trace()
+        urwid.connect_signal(self.contents['footer'][0].search_but, 'click', self.set_search_mode)
+        urwid.connect_signal(self.contents['footer'][0].home_but, 'click', self.set_show_mode)
+
     def keypress(self, size, key):
         if key == 'up':
             self.focus_position = 'body'
@@ -69,6 +75,14 @@ class TopFrame(urwid.Frame):
             self.focus_position = 'footer'
         else:
             return self.focus.keypress(size, key)
+
+    def set_search_mode(self, button):
+        body = SearchBox()
+        self.contents['body'] = (body, body.options())
+
+    def set_show_mode(self, button):
+        body = AppBox()
+        self.contents['body'] = (body, body.options())
 
 
 db = pairsubs.SubDb()
